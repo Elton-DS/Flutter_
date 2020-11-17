b4df702f09396c570805d53fd59c1d49b2d8a364
Elton Martins <elton_815@hotmail.com>
2020-11-17 12:10:10 -0300

    Criado as pastas app/pages/controlles

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'app/pages/home_page.dart';
+
 
 void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Home(),
   ));
-}
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Projeto Git'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            FlutterLogo(
-              size: 120.0,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 }
