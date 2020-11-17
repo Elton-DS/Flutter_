5e6baa053a035726befbba56eb5d9f4d1eea204e
Elton Martins <elton_815@hotmail.com>
2020-11-17 20:01:21 -0300

    Criaçao controller

--- lib/app/pages/contador.dart
+++ lib/app/pages/contador.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:projeto_git/app/controllers/controller.dart';
 
 class Contador extends StatelessWidget {
+  Controller controller = Get.put(Controller());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text( 'Contador'),
       ),
+      body: Obx(()=>ListView.builder(
+        itemCount: controller.list.length,
+        itemBuilder:(__,index)=>ListTile(
+          title: Text(controller.list[index]),
+        ))),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            controller.adicionarItens();
+          },
+          child: Icon(Icons.add),
+        ),
     );
   }
 }
